b2cdd3e9f882930b6d59f47ce78eae8006ca574c
PotatoDistribution <60849417+PotatoDistribution@users.noreply.github.com>
2022-01-11 13:44:25 -0500

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,23 +1,24 @@
-package GroupProject;
-
 public class Vehicle
 {
     float price;
     short year;
+    int mileage;
     String name, plate;
     
     public Vehicle(){
         price = 0.00f;
         year = 0;
+        mileage = 0;
         name = null;
         plate = null;
     }
     
-    public Vehicle(float price, short year, String name, String plate){
+    public Vehicle(float price, short year, String name, String plate, int mileage){
         this.price = price;
         this.year = year;
         this.name = name;
         this.plate = plate;
+        this.mileage = mileage;
     }
     
     public String toString(){
@@ -26,7 +27,7 @@ public class Vehicle
         r+="Plate: "+plate;
         r+="Year: "+year;
         r+="Price: "+price;
+        r+="Mileage: "+mileage;
         return r;
     }
 }
-
