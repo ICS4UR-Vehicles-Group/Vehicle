bfea31167f3ebc2cd080b55d3df49bf9e0132873
PotatoDistribution <60849417+PotatoDistribution@users.noreply.github.com>
2022-01-12 13:51:23 -0500

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,26 +1,32 @@
 public class Vehicle
 {
-    float price;
+    //Instance variables
+    float price, residualValue;
     short year;
     int mileage;
     String name, plate;
     
+    //Default contructor
     public Vehicle(){
         price = 0.00f;
+        residualValue = 0.0f;
         year = 0;
         mileage = 0;
         name = null;
         plate = null;
     }
     
-    public Vehicle(float price, short year, String name, String plate, int mileage){
+    //Constructor
+    public Vehicle(float price, float residualValue, short year, String name, String plate, int mileage){
         this.price = price;
+        this.residualValue = residualValue;
         this.year = year;
         this.name = name;
         this.plate = plate;
         this.mileage = mileage;
     }
     
+    //toString
     public String toString(){
         String r = "";
         r+="Name: "+name;
@@ -30,11 +36,12 @@ public class Vehicle
         r+="Mileage: "+mileage;
         return r;
     }
-    
+
+    //Add distance
     public void drive(int distance){
         mileage += distance;
     }
-    
+
     //Accident decreases value fo car
     public void accident(float decrease){
         residualValue -= decrease;
