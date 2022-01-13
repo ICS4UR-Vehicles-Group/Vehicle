2fc715b3a40ca72323ecb2f718c5269b69d0459a
abath1 <60665110+abath1@users.noreply.github.com>
2022-01-13 13:01:41 -0500

    Update Dealership.java

--- Dealership.java
+++ Dealership.java
@@ -4,17 +4,20 @@ public class Dealership {
     String name, address;
     //Tree vehicles
     byte vehiclesNum;
+    float profit;
 
     public Dealership(){
         name = null;
         address = null;
         vehiclesNum = 0;
+        profit = 0.0f;
     }
 
     public Dealership(String name, String address, byte n){
         this.name = name;
         this.address = address;
         vehiclesNum = n;
+        profit = 0.0f;
     }
 
     public String toString(){
