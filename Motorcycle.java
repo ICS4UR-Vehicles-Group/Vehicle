c19a9b21faf57de6c841968435a54b3afbc713a6
MMMMMMMNMMMMMMM <62734500+MMMMMMMNMMMMMMM@users.noreply.github.com>
2022-01-13 12:53:32 -0500

    Update Motorcycle.java

--- Motorcycle.java
+++ Motorcycle.java
@@ -6,41 +6,32 @@ public class Motorcycle extends Vehicle {
 	float enginedisplacement;
 	boolean istwostroke;
 	
-	
 	public Motorcycle() {
+		super();
 		String type, engine = null;
 		float enginedisplacement = 0.0f;
 		boolean istwostroke = true;
 	}
 	
 	public Motorcycle(String type, String engine, float enginedisplacement, boolean istwostroke) {
+		super();
 		this.type = type;
 		this.engine = engine;
 		this.enginedisplacement = enginedisplacement;
 		this.istwostroke = istwostroke;
 	}
 	
-	public static void main(String args[]) {
-		
-		Motorcycle m1 = new Motorcycle("Dirtbike", "V4", 5.4f, true);
-		
-		m1.mileage = 100;
-		
-		System.out.println(m1);
-		
-	}
-	
 	public String toString() {
 		
 		String r = super.toString();
-		r+="\nType: "+type;
-        r+="\nEngine: "+engine;
-        r+="\nEnginedisplacement: "+enginedisplacement;
-        if(istwostroke) {
-        	r+="\n2 Stroke";
-        }
-        else 
-        	r+="\n4 Stroke";
+		r+="\nTypse: "+type;
+        	r+="\nEngine: "+engine;
+        	r+="\nEnginedisplacement: "+enginedisplacement;
+        	if(istwostroke) {
+        		r+="\n2 Stroke";
+        	}
+		else 
+        		r+="\n4 Stroke";
 		return r;
 	}
 	
